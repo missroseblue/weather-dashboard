497b9fdb11fd8450379aef6faaedfabcc2cb04d7
missroseblue <missroseblue@gmail.com>
2021-09-19 18:19:27 -0700

    added daily forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ var cityInputEl = document.querySelector("#cityNameInput");
 var cityContainerEl = document.querySelector("#conditions-container");
 var citySearchTerm = document.querySelector("#conditions-search-term");
 var currentConditionsEl = document.querySelector(".cityCurrentConditions");
-   
+var weeklyForecastEL = document.querySelector(".weeklyForecast");
+
 
 
 var formSubmitHandler = function (event) {
@@ -57,57 +58,93 @@ var getCity = function (city) {
     currentConditionsEl.appendChild(cityNameTitle);
 
   })
-    // .catch(function (error) {
-    //   //Notice this '.catch' getting chained onto the end of the 'then()' method
-    //   console.log(error);
-    //   alert("Unable to connect to OpenWeather");
-    // });
+  // .catch(function (error) {
+  //   //Notice this '.catch' getting chained onto the end of the 'then()' method
+  //   console.log(error);
+  //   alert("Unable to connect to OpenWeather");
+  // });
 }
 
 //fetch coordinates api
 function oneCall(lat, lon) {
-  var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon=' + lon + '&units=imperial&appid=23d40c242866a0b133662ed347c4e80e'
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=23d40c242866a0b133662ed347c4e80e'
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       return response.json()
-    } else { 
-    console.log("oneCall error")
+    } else {
+      console.log("oneCall error")
     }
   }).then(function (data) {
     console.log(data);
-  
-    for (i=0; i< 5; i++) {
+
+    for (i = 0; i < 5; i++) {
       console.log(data.daily[i].temp.day);
-  
+
     }
-       //display current conditions
-   
-   var cityTemp = document.createElement("p");
-   cityTemp.innerHTML = "Temp: " + data.current.temp;
-   currentConditionsEl.appendChild(cityTemp);
+    //display current conditions
+    var currentTemp = document.createElement("p");
+    currentTemp.innerHTML = "Temp: " + Math.ceil(data.current.temp) + "° F";
+    currentConditionsEl.appendChild(currentTemp);
 
-   var cityHumidity = document.createElement("p");
-   cityHumidity.innerHTML = "Humidity: " + data.current.humidity + "%";
-   currentConditionsEl.appendChild(cityHumidity);
-   
-   var cityWind = document.createElement("p");
-   cityWind.innerHTML = "Wind: " + Math.ceil(data.current.wind_speed) + " mph";
-   currentConditionsEl.appendChild(cityWind);
+    var currentHumidity = document.createElement("p");
+    currentHumidity.innerHTML = "Humidity: " + data.current.humidity + "%";
+    currentConditionsEl.appendChild(currentHumidity);
 
-   var cityUV = document.createElement("p");
-   cityUV.innerHTML = "UV Index: " + data.current.uvi;
-   currentConditionsEl.appendChild(cityUV);
-  
-  }).catch(function (error) {
-    console.log(error);
-    alert("Unable to connect to OpenWeather");
-  });
-  
+    var currentWind = document.createElement("p");
+    currentWind.innerHTML = "Wind: " + Math.ceil(data.current.wind_speed) + " mph";
+    currentConditionsEl.appendChild(currentWind);
 
+    var currentUV = document.createElement("p");
+    currentUV.innerHTML = "UV Index: " + Math.ceil(data.current.uvi);
+    currentConditionsEl.appendChild(currentUV);
 
+ if (data.current.uvi <= 2) {
+      currentUV.setAttribute("class","fs-6 rounded-3 text-light bg-success");
+     } 
+     else if (data.current.uvi < 5) {
+      currentUV.setAttribute("class","fs-6 rounded-3 text-light bg-warning");
+     }
+     else if (data.current.uvi < 7) {
+      currentUV.setAttribute("class","fs-6 rounded-3 text-light bg-orange");
+     }
+     else if (data.current.uvi < 10) {
+      currentUV.setAttribute("class","fs-6 rounded-3 text-light bg-danger")
+     }
+     else {
+      currentUV.setAttribute("class","fs-6 rounded-3 text-light bg-purple");
+     }
+    
+      weeklyForecastEL.classList.remove("hidden")
+      let dayCards = ``
 
-  }
+      for (var i = 0; i < 5; i++) {
+        var day = dayjs().add(i + 1, 'day').format('MMM-D-YYYY')
+        var symbol = data.daily[i].weather[0].icon
+        var temp = data.daily[i].temp.day
+        var wind = data.daily[i].wind_speed
+        var humidity = data.daily[i].humidity
 
+       
+        var newCard = `
+        <div class="card col">
+        <div class="card-body">
+        <p> Date: ${day}</p>
+        <img src="http://openweathermap.org/img/wn/${symbol}.png" /> 
+        <p> Temp: ${temp}</p>
+        <p> Wind: ${wind}</p>
+        <p> Humidity: ${humidity}</p>
+        </div></div>
+        `
+        
+      dayCards += newCard
+    }
+weeklyForecastEL.innerHTML = dayCards
+  })
+  // }).catch(function (error) {
+  //   console.log(error);
+  //   alert("Unable to connect to OpenWeather");
+  // });
+};
 
 
 //add event listeners to the form
@@ -122,111 +159,3 @@ locationButtonsEL.addEventListener("click", buttonClickHandler);
 
 
 
-// var displayWeather = function (conditions, searchTerm) {
-//   //check if api returned any repos
-//   if (conditions.length === 0) {
-//     cityContainerEl.textContent = "No city found.";
-//   }
-
-//   console.log(conditions);
-//   console.log(searchTerm);
-
-//   //clear old content
-//   cityContainerEl.textContent = "";
-//   citySearchTerm.textContent = searchTerm;
-
-//   //format condition name
-//   var conditionsName = conditions[i].owner.login + "/" + conditions[i].name;
-
-//   //create a container for each conditions
-//   var conditionsEl = document.createElement("div");
-//   conditionsEl.classList = "list-item flex-row justify-space-between align-center";
-
-//   //create a span element to hold conditions name
-//   var titleEl = document.createElement("span");
-//   titleEl.textContent = conditionsName;
-
-//   //apend to container 
-//   conditionsEl.appendChild(titleEl);
-
-//   //append container to the dom
-//   conditionsContainerEl.appendChild(conditionsEl)
-
-
-// };
-
-
-
-
-
-
-/*
-
-api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon=' + lon + '&units=imperial&appid=23d40c242866a0b133662ed347c4e80e'
-
-'https://api.openweathermap.org/data/2.5/weather?q=' + city +"&appid=23d40c242866a0b133662ed347c4e80e"
-
-var getCity = function (cityNameInput) {
-  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=" + cityNameInput +"&units=imperial&appid=23d40c242866a0b133662ed347c4e80e'
-fetch(weatherUrl)
-  .then(response => response.json())
-  .then(response) => {
-    var responseContainerEL = $("#response-container"),
-    var cityWeather = $("<div />").attr("src", response.city.name);
-    responseContainerEL.append(cityWeather);
-  };
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?q={cityNameInput}&appid=23d40c242866a0b133662ed347c4e80e')
-.then((res) => res.json())
-.then((res) => console.log(res))
-
-fetch('api.openweathermap.org/data/2.5/forecast?q=London&appid=23d40c242866a0b133662ed347c4e80e')
-.then((res) => res.json())
-.then((res) => console.log(res))
-//
-//});*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var responseContainerEL = $("#response-container"),
-    var cityWeather = $("<div />").attr("src", {'city'});
-    responseContainerEL.append(cityWeather);
-
-//link submitted city to API
-
-
-
-//Add event listeners to the city buttons
-var getCity = function() {
-  // format the github weather url
-  var weatherUrl = "api.openweathermap.org/data/2.5/weather?q=London&appid=3db667141051263a8c63655d71fb8aef"
-  //"api.openweathermap.org/data/2.5/weather?q=" + cityNameInput +"&appid=3db667141051263a8c63655d71fb8aef"
-
-
-  // make a request to the url
-  fetch(weatherUrl).then(function(response) {
-  response.json().then(function(data) {
-  console.log(data);
-    });
-  });
-};
-*/
